2a8464393abcfbbf3997c74310ef089a24e6c316
Valéria <106544831+valeria-qf@users.noreply.github.com>
2022-08-09 23:33:36 -0300

    Update q9.cpp

--- matrix/q9.cpp
+++ matrix/q9.cpp
@@ -1,7 +1,7 @@
 /*Elaborar um programa que leia uma matriz A do tipo inteira de duas dimensões com sete
 linhas e sete colunas. Construir a matriz B de mesma dimensão, em que cada elemento seja
 o somatório de 1 até o valor armazenado na posição da matriz A, com exceção dos valores
-situados nos índices ímpares da diagonal principal (B[1,1], B[3,3], B[5,5] e B[7,7]), os quais
+situados nos índices da diagonal principal (B[1,1], B[3,3], B[5,5] e B[7,7]), os quais
 devem ser o fatorial de cada elemento correspondente da matriz A. Apresentar ao final a
 matriz B.*/
 

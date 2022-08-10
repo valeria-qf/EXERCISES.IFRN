1090e2f02d8a027e659ae1aead939818ad2d648b
Valéria <106544831+valeria-qf@users.noreply.github.com>
2022-08-10 00:02:13 -0300

    Update q9.cpp

--- matrix/q9.cpp
+++ matrix/q9.cpp
@@ -13,9 +13,9 @@ int main()
 
      srand((unsigned)time(NULL));
     
-    for (int i=0; i<5; i++)
+    for (int i=0; i<7; i++)
     {
-        for (int j=0; j<5; j++)
+        for (int j=0; j<7; j++)
         {
             A[i][j]=rand()%10;
             cout<<A[i][j]<<" | ";
@@ -23,9 +23,9 @@ int main()
         cout<<endl;
     }
     cout<<endl;
-      for (int i=0; i<5; i++)
+      for (int i=0; i<7; i++)
     {
-        for (int j=0; j<5; j++)
+        for (int j=0; j<7; j++)
         {
             if(i==j)
             {
